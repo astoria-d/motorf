38d48b33d39f8d13b5bdc260b6eb162cb4439e00
astoria-d <astoria-d@home>
2020-03-08 21:39:43 +0900

    - Add time constraints
    - Add led and sw

--- prfx1_test01/prfx1_test01.vhd
+++ prfx1_test01/prfx1_test01.vhd
@@ -1,16 +1,14 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
---  
---   MOTO NES FPGA On GHDL Simulation Environment Virtual Cuicuit Board
---   All of the components are assembled and instanciated on this board.
---  
-
 entity prfx1_test01 is 
    port (
-	signal clk     : in std_logic;
-	signal sw     	: in std_logic;
-	signal led1		: out std_logic
+	signal in_clk     : in std_logic;
+	signal sw1     	: in std_logic;
+	signal sw2     	: in std_logic;
+	signal led1		: out std_logic;
+	signal led2		: out std_logic;
+	signal led3		: out std_logic
 	);
 end prfx1_test01;
 
@@ -18,10 +16,12 @@ architecture rtl of prfx1_test01 is
 
 begin
 
-   LED_set_p : process (clk)
+   LED_set_p : process (in_clk)
    begin
-		if (rising_edge(clk)) then
-			LED1 <= SW;
+		if (rising_edge(in_clk)) then
+			led1 <= sw1;
+			led2 <= sw2;
+			led3 <= '1';
 		end if;
 	end process;
 
