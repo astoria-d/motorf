7a780f6729f3868693df4136eb833482fb428607
astoria-d <astoria-d@home>
2020-03-08 22:16:37 +0900

    add 80MHz pll

--- prfx1_test01/prfx1_test01.vhd
+++ prfx1_test01/prfx1_test01.vhd
@@ -14,6 +14,16 @@ end prfx1_test01;
 
 architecture rtl of prfx1_test01 is
 
+component PLL
+	PORT
+	(
+		inclk0	: IN STD_LOGIC  := '0';
+		c0			: OUT STD_LOGIC 
+	);
+END component;
+
+signal clk80m  : std_logic;
+
 begin
 
    LED_set_p : process (clk16m)
@@ -21,9 +31,14 @@ begin
 		if (rising_edge(clk16m)) then
 			led1 <= sw1;
 			led2 <= sw2;
-			led3 <= '1';
+			led3 <= '0';
 		end if;
 	end process;
+
+	PLL_inst : PLL PORT MAP (
+		inclk0	=> clk16m,
+		c0	 		=> clk80m
+	);
 
 end rtl;
 
