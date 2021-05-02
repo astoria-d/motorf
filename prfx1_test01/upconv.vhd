62ab5ae018a4f5363b1b5bf4e9f1fce6a6843cdc
astoria-d <astoria-d@home>
2021-05-02 23:28:19 +0900

    correct upcomv

--- prfx1_test01/upconv.vhd
+++ prfx1_test01/upconv.vhd
@@ -40,13 +40,10 @@ END component;
 
 signal sin : std_logic_vector(15 downto 0);
 signal cos : std_logic_vector(15 downto 0);
+signal add_sin : std_logic_vector(31 downto 0);
+signal add_cos : std_logic_vector(31 downto 0);
 
 begin
-
-	if_i <= bb_i;
-	if_q <= bb_q;
---	if_i <= sin;
---	if_q <= cos;
 
 --	--NCO instance
 --	NCO_1MHz : MY_NCO PORT MAP (
@@ -55,13 +52,28 @@ begin
 --		sin	=> sin,
 --		cos	=> cos
 --	);
-	--NCO instance
+	--NCO instance, 875 KHz wave
 	NCO_1MHz : moto_nco PORT MAP (
 		clk	=> clk80m,
-		frq   => conv_std_logic_vector(53687091, 32),
+		frq   => conv_std_logic_vector(46976205, 32),
 		sin	=> sin,
 		cos	=> cos
 	);
 
+	--add 875 KHz + 125 KHz, create 1 MHz wave
+   upconv_p : process (clk16m)
+   begin
+		if (rising_edge(clk16m)) then
+			--wave addition theorem
+			add_sin <= cos * bb_i - sin * bb_q;
+			add_cos <= sin * bb_i + cos * bb_q;
+			if_i <= add_sin(31 downto 16);
+			if_q <= add_cos(31 downto 16);
+--			if_i <= sin;
+--			if_q <= cos;
+--			if_i <= bb_i;
+--			if_q <= bb_q;
+		end if;
+	end process;
 
 end rtl;
