4ebe6f2649682c34214316286cea47897559cc90
astoria-d <astoria-d@home>
2022-07-19 19:53:57 +0900

    rx uart debugging

--- prfx1_test06_uart/prfx1_test06_uart.vhd
+++ prfx1_test06_uart/prfx1_test06_uart.vhd
@@ -42,6 +42,15 @@ component uart_out
 	);
 end component;
 
+component demodulator_dummy
+	port (
+	signal reset_n 	: in std_logic;
+	signal clk80m		: in std_logic;
+	signal out_en		: out std_logic;
+	signal out_word 	: out std_logic_vector(7 downto 0)
+	);
+end component;
+
 signal reset_n : std_logic;
 
 signal clk80m		: std_logic;
@@ -49,11 +58,10 @@ signal clk40m		: std_logic;
 signal clk12m		: std_logic;
 signal clk5m     	: std_logic;
 
-signal uart_data	: std_logic_vector(7 downto 0);
-signal uart_data_en	: std_logic;
+signal baseband_data	: std_logic_vector(7 downto 0);
+signal baseband_data_en	: std_logic;
 
-signal uart_clk_cnt : std_logic_vector (10 downto 0);
-constant input_data_div		: integer := 196 * 8;
+
 
 begin
 
@@ -69,10 +77,17 @@ begin
 		c3	 		=> clk5m
 	);
 
+	demodulator_dummy_inst : demodulator_dummy PORT MAP (
+		reset_n 			=> reset_n,
+		clk80m			=> clk80m,
+		out_en			=> baseband_data_en,
+		out_word			=> baseband_data
+	);
+
 	uart_out_inst : uart_out PORT MAP (
 		clk80m			=> clk80m,
-		uart_en			=> uart_data_en,
-		uart_data		=> uart_data,
+		uart_en			=> baseband_data_en,
+		uart_data		=> baseband_data,
 		uart_txd			=> ftdi_txd
 	);
 
@@ -94,43 +109,104 @@ begin
 		end if;
 	end process;
 
+end rtl;
+
+
+--------------------
+--------------------
+--------------------
+--------------------
+
+
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
+use ieee.std_logic_unsigned.all;
+
+entity demodulator_dummy is
+	port (
+	signal reset_n 	: in std_logic;
+	signal clk80m		: in std_logic;
+	signal out_en		: out std_logic;
+	signal out_word 	: out std_logic_vector(7 downto 0)
+	);
+end demodulator_dummy;
+
+architecture rtl of demodulator_dummy is
+
+signal clk_cnt : std_logic_vector (10 downto 0);
+constant input_data_div		: integer := 196 * 8;
+
+constant uart_start_time : time := 10 us;
+signal uart_start : std_logic;
+
+signal baseband_data	: std_logic_vector(7 downto 0);
+signal baseband_data_en	: std_logic;
+
+begin
+
+	out_en <= baseband_data_en;
+
+	--- start testing.
+--	uart_data_in: process
+--	begin
+--		uart_start <= '0';
+--		wait for uart_start_time;
+--		uart_start <= '1';
+--		wait;
+--	end process;
+	uart_start <= '1';
+
 	uart_clk_p : process (clk80m)
 	begin
 		if (rising_edge(clk80m)) then
 			if (reset_n = '0') then
-				uart_clk_cnt <= (others => '0');
+				clk_cnt <= (others => '0');
 			else
-				if (uart_clk_cnt = input_data_div) then
-					uart_clk_cnt <= (others => '0');
+				if (clk_cnt = input_data_div) then
+					clk_cnt <= (others => '0');
 				else
-					uart_clk_cnt <= uart_clk_cnt + 1;
+					clk_cnt <= clk_cnt + 1;
 				end if;
 			end if;
 		end if;
 	end process;
 
-	uart_data_p : process (clk80m)
+	baseband_data_p : process (clk80m)
 	begin
 		if (rising_edge(clk80m)) then
-			if (reset_n = '0') then
-				uart_data <= (others => '0');
+			if (reset_n = '0' or uart_start = '0') then
+				baseband_data <= (others => '0');
 			else
-				if (uart_clk_cnt = 0) then
-					uart_data <= uart_data + 1;
+				if (clk_cnt = 0) then
+					baseband_data <= baseband_data + 1;
+					if (baseband_data(2) = '0') then
+						out_word <= (others => '0');
+					else
+						out_word <= baseband_data;
+					end if;
 				end if;
 			end if;
 		end if;
 	end process;
 
-	uart_en_p : process (clk80m)
+	baseband_data_en_p : process (clk80m)
 	begin
 		if (rising_edge(clk80m)) then
-			if (uart_clk_cnt = 1) then
-				uart_data_en <= '1';
+			if (reset_n = '0' or uart_start = '0') then
+				baseband_data_en <= '0';
 			else
-				uart_data_en <= '0';
+				if (clk_cnt = 1) then
+					baseband_data_en <= '1';
+				else
+					baseband_data_en <= '0';
+				end if;
 			end if;
 		end if;
 	end process;
+
 
 end rtl;
+
+
--- prfx1_test06_uart/simulation/modelsim/prfx1_test06_uart_run_msim_rtl_vhdl.do
+++ prfx1_test06_uart/simulation/modelsim/prfx1_test06_uart_run_msim_rtl_vhdl.do
@@ -16,18 +16,22 @@ vsim work.prfx1_test06_uart_testbench
 add wave -label clk16m sim:/prfx1_test06_uart_testbench/sim_board/clk16m
 add wave -label clk80m sim:/prfx1_test06_uart_testbench/sim_board/clk80m
 
-add wave -label uart_clk_cnt -radix unsigned sim:/prfx1_test06_uart_testbench/sim_board/uart_clk_cnt
+add wave -divider baseband_data
+add wave -label clk_cnt -radix unsigned sim:/prfx1_test06_uart_testbench/sim_board/demodulator_dummy_inst/clk_cnt
+add wave -label baseband_data_en  sim:/prfx1_test06_uart_testbench/sim_board/demodulator_dummy_inst/baseband_data_en
+#add wave -label baseband_data -radix hex  sim:/prfx1_test06_uart_testbench/sim_board/demodulator_dummy_inst/baseband_data
+add wave -label out_word -radix hex  sim:/prfx1_test06_uart_testbench/sim_board/demodulator_dummy_inst/out_word
 
-add wave -label uart_data_en  sim:/prfx1_test06_uart_testbench/sim_board/uart_data_en
-add wave -label uart_data -radix hex  sim:/prfx1_test06_uart_testbench/sim_board/uart_data
 
-
+add wave -divider uart
 add wave -label cur_state  sim:/prfx1_test06_uart_testbench/sim_board/uart_out_inst/cur_state
 add wave -label reg_uart_data  -radix hex  sim:/prfx1_test06_uart_testbench/sim_board/uart_out_inst/reg_uart_data
 add wave -label uart_txd  sim:/prfx1_test06_uart_testbench/sim_board/uart_out_inst/uart_txd
 
 
-run 50us
+run 100us
 
 wave zoom full
+
+run 200us
 
